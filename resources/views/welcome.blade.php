6a51120ad252c0a2699c6de9920b1d5d99a339e6
Jesrel <jesrelgoncalves@gmail.com>
2019-03-29 21:07:58 -0300

    Alteração tela de boas vindas

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel 29/03/19
                 </div>
 
                 <div class="links">
